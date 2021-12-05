62d53de122490e8ce89d6bc486c4940301453077
Valerie Tonsor <30914395+valerietonsor@users.noreply.github.com>
2021-12-05 13:29:04 -0600

    added disclaimer text

--- script.js
+++ script.js
@@ -4,9 +4,7 @@ let comment2 = "It’s true, at today’s savings interest rates, you would neve
 let comment3 = "You will double your money in only 9 years! Since compound interest is not a linear relationship, you get a larger amount of return each year that you save. You can see from the graph that you make your next $1000 at year 14, only 5 additional years of saving."; 
 let comment4 = "Compound interest turned the $1000 into $47,000 over time. WOW! If you waited 60 years, compound interest would return $101,000! Those ten years meant a change of $55,000!"; 
 let comment5 = "A brand new car loses value as soon as it is purchased and then it continues to lose value each year; this process is called depreciation. As a general rule, a new vehicle loses about 20% of its value in the first year and then about 10% of its value every year that follows. Good financial advice is to buy a used car so that you get the benefit of depreciation when you negotiate the price.";
-
 let comment6 = "A brand new cell phone loses value as soon as it is purchased and it continues to depreciate rapidly- most phones will lose more than half their value within 2 years. Most tech items (cell phones, computers, gaming systems) lose value daily, so there is substantial savings to be had when buying used."; 
-
 let comment7 = "I know, right!? In this situation, the phone actually cost you $1921: $999 for the phone and $921 in interest to the credit card company. Each time you use a credit card, you are taking out a short-term loan with a high interest rate; only paying the monthly minimum balance means that compound interest is working against you, and at a more rapid pace than most investments can match. Paying down credit card debt is always a great financial decision."
 
 let correctAnswer2 = "7000 years"; 
@@ -16,7 +14,7 @@ let correctAnswer5 = "$10,500";
 let correctAnswer6 = "$480"; 
 let correctAnswer7 = "113 months (9 years, 5 months)"; 
 
-
+let disclaimer = `It's commonly said that "Past performance is no guarantee of future results" and that is absolutely true. Investments with higher potential for return (like stocks, cryptocurrencies, etc.) come with higher risk; sometimes their values will go up and sometimes they will go down. Our questions assumed a guaranteed rate of return, but with real-life investments there is no guarantee. When investing your own money, choose a risk level that is comfortable for you, and only invest money that you can comfortably see decrease in the short and long-term.`;
 
 function one(){ 
 document.getElementById("nextPageBtn").style.visibility = "visible"; 
